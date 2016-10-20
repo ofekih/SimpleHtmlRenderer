a707c554e462b1487f788d9ff82b7ca40fe045ea
Ofek Gila <ofek@theofekfoundation.org>
2016-10-20 14:39:40 -0700

    Initial commit

---
+++ Tester.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class Tester {
+	public static void main(String... pumpkins) {
+		SimpleBrowser frame = new SimpleBrowser();
+		frame.createWindow(500, 30);
+	}
+}
