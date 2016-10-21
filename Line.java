7d0ea511a48a0c63b86751c6fb72d2712012c7c3
Ofek Gila <ofek@theofekfoundation.org>
2016-10-20 19:16:25 -0700

    Add javadoc for line

--- Line.java
+++ Line.java
@@ -1,3 +1,7 @@
+/**
+ * A single line object containing the text, font, and color of the line
+ */
+
 import java.awt.Font;
 import java.awt.Color;
 import java.awt.FontMetrics;
@@ -10,6 +14,13 @@ public class Line {
 	private Color color;
 	private FontMetrics fontMetrics;
 
+	/**
+	 * Line constructor with all the necessary values
+	 * @param  text      the text to display
+	 * @param  font      the {@link Font} to use
+	 * @param  color     the {@link Color} to use
+	 * @param  component any old {@link JComponent} to get font metrics from
+	 */
 	public Line(String text, Font font, Color color, JComponent component) {
 		this.text = text;
 		this.font = font;
@@ -17,22 +28,42 @@ public class Line {
 		fontMetrics = component.getFontMetrics(font);
 	}
 
+	/**
+	 * Gets the text of this line
+	 * @return the text
+	 */
 	public String getText() {
 		return text;
 	}
 
+	/**
+	 * Gets the font of this line
+	 * @return the {@link Font}
+	 */
 	public Font getFont() {
 		return font;
 	}
 
+	/**
+	 * Gets the color of this line
+	 * @return the {@link Color}
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Gets the line height of this line
+	 * @return the height of this line
+	 */
 	public int getLineHeight() {
 		return fontMetrics.getHeight();
 	}
 
+	/**
+	 * Gets how high below the text is offset
+	 * @return the ascent
+	 */
 	public int getAscent() {
 		return fontMetrics.getAscent();
 	}
